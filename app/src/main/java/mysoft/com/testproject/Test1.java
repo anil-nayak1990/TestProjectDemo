2ceafdff289af3613b35f142aa874659e194cb92
Anil Nayak <nayak1.anil@gmail.com>
2017-12-06 16:11:04 +0530

    new Java file created

---
+++ app/src/main/java/mysoft/com/testproject/Test1.java
@@ -0,0 +1,18 @@
+package mysoft.com.testproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by apple on 06/12/17.
+ */
+
+public class Test1 extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_test);
+    }
+}
